2619b38a7ba17c0861bedfcff806c74d20882d58
Vin-it-9 <vineetshinde2559@gmail.com>
2024-12-28 12:23:33 +0530

    Commit

---
+++ demo/src/main/java/User/UserRepository.java
@@ -0,0 +1,12 @@
+package User;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepository<User> {
+
+
+
+
+}
